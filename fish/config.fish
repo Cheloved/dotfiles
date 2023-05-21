0b093a2c4c9c5b5a90f6103e36aab9debbfe6416
Cheloved <27046990+Cheloved@users.noreply.github.com>
2023-05-21 13:21:22 +0300

    Update

--- fish/config.fish
+++ fish/config.fish
@@ -1,11 +1,3 @@
-set -gx FZF_DEFAULT_OPTS "--multi --height=50% --margin=5%,2%,2%,5% --layout=reverse-list --border=double --info=inline --prompt='\$>' --pointer='→' --marker='♡' --header='CTRL-c or ESC to quit' --color='dark,fg:magenta'"
-
-alias r="ranger"
-alias nv="neovide&"
-alias sd "cd ~ && cd (find * -type d | fzf)"
-
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
-
-
