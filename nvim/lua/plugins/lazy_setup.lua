a62bb51963b72ad79714929a2f0ffadae1a2134b
Cheloved <27046990+Cheloved@users.noreply.github.com>
2023-05-23 18:31:25 +0300

    Bar for buffers added

--- nvim/lua/plugins/lazy_setup.lua
+++ nvim/lua/plugins/lazy_setup.lua
@@ -83,6 +83,23 @@ local plugins = {
 
     -- Auto commenting with "gc"
     'tpope/vim-commentary',
+
+    -- Bar for tabs
+    {
+        'romgrk/barbar.nvim',
+        dependencies = {
+            'lewis6991/gitsigns.nvim', -- OPTIONAL: for git status
+            'nvim-tree/nvim-web-devicons', -- OPTIONAL: for file icons
+        },
+        init = function() vim.g.barbar_auto_setup = false end,
+        opts = {
+          -- lazy.nvim will automatically call setup for you. put your options here, anything missing will use the default:
+            animation = true,
+          -- insert_at_start = true,
+          -- …etc.
+        },
+        version = '^1.0.0', -- optional: only update when a new 1.x version is released
+    },
 }
 
 local opts = {}
