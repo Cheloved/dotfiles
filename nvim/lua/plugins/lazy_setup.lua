5134bdba7f18b5eef7927e00b81afa08bf8cb46d
Cheloved <27046990+Cheloved@users.noreply.github.com>
2025-01-28 01:57:23 +0300

    Neovim tabbar changed to Tabby(shows actual tabs instead of buffers)

--- nvim/lua/plugins/lazy_setup.lua
+++ nvim/lua/plugins/lazy_setup.lua
@@ -120,23 +120,32 @@ local plugins = {
     'tpope/vim-commentary',
 
     -- Bar for tabs
+    -- {
+    --     'romgrk/barbar.nvim',
+    --     dependencies = {
+    --         'lewis6991/gitsigns.nvim', -- OPTIONAL: for git status
+    --         'nvim-tree/nvim-web-devicons', -- OPTIONAL: for file icons
+    --     },
+    --     init = function() vim.g.barbar_auto_setup = false end,
+    --     opts = {
+    --       -- lazy.nvim will automatically call setup for you. put your options here, anything missing will use the default:
+    --         animation = true,
+    --         icons = {
+    --             button = '󱎘',
+    --         },
+    --       -- insert_at_start = true,
+    --       -- …etc.
+    --     },
+    --     version = '^1.0.0', -- optional: only update when a new 1.x version is released
+    -- },
+
     {
-        'romgrk/barbar.nvim',
-        dependencies = {
-            'lewis6991/gitsigns.nvim', -- OPTIONAL: for git status
-            'nvim-tree/nvim-web-devicons', -- OPTIONAL: for file icons
-        },
-        init = function() vim.g.barbar_auto_setup = false end,
-        opts = {
-          -- lazy.nvim will automatically call setup for you. put your options here, anything missing will use the default:
-            animation = true,
-            icons = {
-                button = '󱎘',
-            },
-          -- insert_at_start = true,
-          -- …etc.
-        },
-        version = '^1.0.0', -- optional: only update when a new 1.x version is released
+        'nanozuki/tabby.nvim',
+        -- event = 'VimEnter', -- if you want lazy load, see below
+        dependencies = 'nvim-tree/nvim-web-devicons',
+        config = function()
+            -- configs...
+        end,
     },
 
     -- For color preview
